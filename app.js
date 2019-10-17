56570f1f78e72f1b5a596521089bbeaf5d5dc749
Shikeless <Shikeless@gmail.com>
2019-10-17 17:42:34 +0300

    working with multer

--- app.js
+++ app.js
@@ -1,49 +1,32 @@
-const createError = require("http-errors");
-const express = require("express");
-const path = require("path");
-const cookieParser = require("cookie-parser");
+const Koa = require("koa");
+const app = new Koa();
+const router = require("./routes");
+const serve = require("koa-static");
+const koaBody = require("koa-body");
+const Pug = require("koa-pug");
 const low = require("lowdb");
-const logger = require("morgan");
 const FileSync = require("lowdb/adapters/FileSync");
 const adapter = new FileSync("db.json");
 const db = low(adapter);
-const session = require("express-session");
-const flash = require("connect-flash");
 
-var indexRouter = require("./routes/index");
+app.use(
+    koaBody({
+        formidable: {
+            uploadDir: "./public/img/products" // Директория, куда следует сохранить файл
+        },
+        multipart: true
+    })
+);
 
-var app = express();
-
-db.defaults({}).write();
-
-// view engine setup
-app.set("views", path.join(__dirname, "views"));
-app.set("view engine", "pug");
-
-app.use(logger("dev"));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, "public")));
-
-app.use(session({ cookie: { maxAge: 60000 }, secret: "cat" }));
-app.use(flash());
-
-app.use("/", indexRouter);
-
-app.use(function(req, res, next) {
-    next(createError(404));
+const pug = new Pug({
+    viewPath: "./views",
+    basedir: "./views",
+    app: app
 });
 
-// error handler
-app.use(function(err, req, res, next) {
-    // set locals, only providing error in development
-    res.locals.message = err.message;
-    res.locals.error = req.app.get("env") === "development" ? err : {};
+app.use(serve("./public"));
+app.use(router.routes());
 
-    // render the error page
-    res.status(err.status || 500);
-    res.render("pages/error", { error: err.message });
+app.listen(3000, function() {
+    console.log("Server running on https://localhost:3000");
 });
-
-module.exports = app;
--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,11 @@
-var express = require("express");
-var router = express.Router();
-const path = require("path");
+const Router = require("koa-router");
+const router = new Router();
 const low = require("lowdb");
 const FileSync = require("lowdb/adapters/FileSync");
 const adapter = new FileSync("db.json");
 const db = low(adapter);
-const multer = require("multer");
-var flash = require("connect-flash");
+const multer = require("koa-multer");
+const path = require("path");
 
 const upload = multer({
     storage: multer.diskStorage({
@@ -30,94 +29,135 @@ const upload = multer({
     }
 });
 
-/* GET home page. */
-router.get("/", function(req, res, next) {
-    var social = db.get("social").value();
-    var skills = db.get("skills").value();
-    var products = db.get("products").value();
-    res.render("pages/index", {
-        social: social,
-        skills: skills,
-        products: products,
-        msgsemail: req.flash("msgsemail")
-    });
-});
-
-router.post("/", function(req, res, next) {
-    db.get("contacts")
-        .push({
-            name: req.body.name,
-            email: req.body.email,
-            message: req.body.message
-        })
-        .write();
-    req.flash("msgsemail", "message sended");
-    res.redirect("/");
-});
-
-router.get("/login", function(req, res, next) {
-    var social = db.get("social").value();
-    res.render("pages/login", {
-        social: social,
-        msglogin: req.flash("msglogin")
-    });
-});
-
-router.post("/login", function(req, res, next) {
-    db.get("users")
-        .push({ email: req.body.email, password: req.body.password })
-        .write();
-    req.flash("msglogin", "you are loged in");
-    res.redirect("/");
-});
-
-router.get("/admin", function(req, res, next) {
-    res.render("pages/admin", {
-        msgskill: req.flash("msgskill"),
-        msgsfile: req.flash("msgsfile")
-    });
-});
-
-router.post("/admin/skills", function(req, res, next) {
-    if (req.body.age !== "") {
-        db.get("skills")
-            .find({ id: 1 })
-            .assign({ number: Number(req.body.age) })
-            .write();
+router.get("/", async (ctx, next) => {
+    try {
+        var social = db.get("social").value();
+        var skills = db.get("skills").value();
+        var products = db.get("products").value();
+        await ctx.render("pages/index", {
+            social: social,
+            skills: skills,
+            products: products
+        });
+    } catch (err) {
+        console.log(err);
+        ctx.status = 400;
+        ctx.body = err;
     }
-    if (req.body.concerts !== "") {
-        db.get("skills")
-            .find({ id: 2 })
-            .assign({ number: Number(req.body.concerts) })
-            .write();
-    }
-    if (req.body.cities !== "") {
-        db.get("skills")
-            .find({ id: 3 })
-            .assign({ number: Number(req.body.cities) })
-            .write();
-    }
-    if (req.body.years !== "") {
-        db.get("skills")
-            .find({ id: 4 })
-            .assign({ number: Number(req.body.years) })
-            .write();
-    }
-    req.flash("msgskill", "information has been changed");
-    res.redirect("/admin");
 });
 
-router.post("/admin/upload", upload.single("photo"), function(req, res, next) {
-    const name = req.file.filename;
-    db.get("products")
-        .push({
-            src: `./public/assets/img/products/${name}`,
-            name: req.body.name,
-            price: req.body.price
-        })
-        .write();
-    req.flash("msgsfile", "new product has been aded");
-    res.redirect("/admin");
+router.post("/", async (ctx, next) => {
+    try {
+        await db
+            .get("contacts")
+            .push({
+                name: ctx.request.body.name,
+                email: ctx.request.body.email,
+                message: ctx.request.body.message
+            })
+            .write();
+        await ctx.redirect("/");
+    } catch (err) {
+        console.log(err);
+        ctx.status = 400;
+        ctx.body = err;
+    }
+});
+
+router.get("/login", async (ctx, next) => {
+    try {
+        var social = db.get("social").value();
+        await ctx.render("pages/login", { social: social });
+    } catch (err) {
+        console.log(err);
+        ctx.status = 400;
+        ctx.body = err;
+    }
+});
+
+router.post("/login", async (ctx, next) => {
+    try {
+        await db
+            .get("users")
+            .push({
+                email: ctx.request.body.email,
+                password: ctx.request.body.password
+            })
+            .write();
+        await ctx.redirect("/");
+    } catch (err) {
+        console.log(err);
+        ctx.status = 400;
+        ctx.body = err;
+    }
+});
+
+router.get("/admin", async (ctx, next) => {
+    try {
+        await ctx.render("pages/admin");
+    } catch (err) {
+        console.log(err);
+        ctx.status = 400;
+        ctx.body = err;
+    }
+});
+
+router.post("/admin/skills", async (ctx, next) => {
+    try {
+        if (ctx.request.body.age !== "") {
+            await db
+                .get("skills")
+                .find({ id: 1 })
+                .assign({ number: Number(ctx.request.body.age) })
+                .write();
+        }
+        if (ctx.request.body.concerts !== "") {
+            await db
+                .get("skills")
+                .find({ id: 2 })
+                .assign({ number: Number(ctx.request.body.concerts) })
+                .write();
+        }
+        if (ctx.request.body.cities !== "") {
+            await db
+                .get("skills")
+                .find({ id: 3 })
+                .assign({ number: Number(ctx.request.body.cities) })
+                .write();
+        }
+        if (ctx.request.body.years !== "") {
+            await db
+                .get("skills")
+                .find({ id: 4 })
+                .assign({ number: Number(ctx.request.body.years) })
+                .write();
+        }
+        await ctx.redirect("/admin");
+    } catch (err) {
+        console.log(err);
+        ctx.status = 400;
+        ctx.body = err;
+    }
+});
+
+const uploader = async (ctx, next) => {
+    try {
+        await upload.single("photo")(ctx, async ctx2 => {
+            await ctx.redirect("/admin");
+        });
+    } catch (err) {
+        await ctx.redirect("/admin");
+    }
+};
+
+router.post("/admin/upload", async (ctx, next) => {
+    try {
+        uploader();
+    } catch (err) {
+        console.log(err);
+        ctx.status = 400;
+        ctx.body = err;
+    }
 });
 
 module.exports = router;
